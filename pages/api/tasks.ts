4d585630016c28397f5778999973d918ff11a522
Eban Sayago <eban.sayago@protonmail.com>
2022-11-27 21:04:30 -0300

    :sparkles: MongoDB

---
+++ pages/api/tasks.ts
@@ -0,0 +1,37 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import { clientPromise } from 'db/client'
+import { ObjectId } from 'mongodb'
+
+type Data = {
+    name: string
+}
+
+const handler = async (
+    req: NextApiRequest,
+    res: NextApiResponse
+) => {
+    console.log('here')
+    
+    try {
+        const client = await clientPromise
+        
+        const db = client.db("tasks")
+ 
+        const user = await db
+            .collection("users")
+            .findOne({
+                _id: new ObjectId('63823713c62c559dda97ceb9')
+            })
+            
+
+        console.log('User =>', user)
+ 
+        // res.json(user.tasks)
+    } catch (e) {
+        console.error(e)
+    }
+    // res.status(200).json({ name: 'John Doe' })
+}
+
+export default handler
