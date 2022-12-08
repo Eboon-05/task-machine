4e3cbcd76524d480315e95eb4caa3b19de2cca6f
Eban Sayago <eban.sayago@protonmail.com>
2022-12-08 12:48:16 -0300

    :bug: Delayed state in storage middleware

--- redux/middlewares/storage.ts
+++ redux/middlewares/storage.ts
@@ -3,24 +3,20 @@ import { Middleware } from "@reduxjs/toolkit"
 import { AppState } from "redux/store"
 
 export const storage: Middleware<{}, AppState> = store => next => action => {
+    // calls all the other middlewares and reducers
+    next(action)
+
+    // then runs its code
     const state = store.getState().task
     const storageState = JSON.parse(localStorage.getItem('tasks')) || null
-
-    // TODO: Run this code after reducer 
-    // console.log('received state =>', state)
-
+    
     if (state.list && state.groups && storageState) {
-        // undefined list and groups - not null storage
+        // defined list and groups - not null storage
         localStorage.setItem('tasks', JSON.stringify({
             list: [
                 ...state.list,
             ],
             groups: state.groups,
         }))
-    } else if (storageState) {
-        // defined list and groups - not null storage
-        // store.dispatch()
     }
-
-    next(action)
 }
