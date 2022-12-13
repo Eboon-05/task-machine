60351270c2ef6cfacd23c76cacb505c00e0c0d28
Eban Sayago <eban.sayago@protonmail.com>
2022-12-13 15:53:36 -0300

    :lipstick: Fixed Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -48,33 +48,40 @@ const Navbar = () => {
 
     return (
         <footer className='w-full'>
-            <nav
-                className='bg-light-gray w-[90%] py-5 px-8 rounded-2xl grid justify-center 
-                items-center justify-items-center relative max-w-[330px] grid-cols-3 font-varela'
-            >
-                {links.map((l, i) => (
-                    <Link
-                        href={l.path}
-                        key={i}
-                        className={classNames({
-                            'grid justify-center justify-items-center items-center':
-                                true,
-                            'opacity-50': route !== l.path,
-                            'grid-cols-2': route === l.path,
-                        })}
-                    >
-                        {l.icon}
-                        {l.path === route ? l.name : null}
-                    </Link>
-                ))}
+            <div className='h-[68px]'></div>
 
-                <Link
-                    href={newLink}
-                    className='bg-dark-blue rounded-[1.25rem] shadow-md p-3 h-14 w-14 absolute top-[6px] -right-8
-                    flex items-center justify-center'
+            <nav
+                className='fixed bottom-2 inset-x-2 w-[90%] mx-auto'
+            >
+                <div
+                    className='bg-light-gray  py-5 px-8 rounded-2xl grid justify-center 
+                    items-center justify-items-center max-w-[400px] grid-cols-3 font-varela
+                    w-full h-full relative mx-auto'
                 >
-                    <PlusIcon className='h-7 w-7 text-white' />
-                </Link>
+                    {links.map((l, i) => (
+                        <Link
+                            href={l.path}
+                            key={i}
+                            className={classNames({
+                                'grid justify-center justify-items-center items-center':
+                                    true,
+                                'opacity-50': route !== l.path,
+                                'grid-cols-2': route === l.path,
+                            })}
+                        >
+                            {l.icon}
+                            {l.path === route ? l.name : null}
+                        </Link>
+                    ))}
+
+                    <Link
+                        href={newLink}
+                        className='bg-dark-blue rounded-[1.25rem] shadow-md p-3 h-14 w-14 absolute top-[6px] -right-8
+                        flex items-center justify-center'
+                    >
+                        <PlusIcon className='h-7 w-7 text-white' />
+                    </Link>
+                </div>
             </nav>
 
             <SearchBar onClose={() => setSearch(false)} active={search} />
